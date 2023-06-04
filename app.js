61ef922260a81cada1ab4ac3308c1abe82b3f648
hsyoodev <hsyoodev@gmail.com>
2023-06-04 22:42:27 +0900

    Line width

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -12,10 +12,9 @@ function movePainting(event) {
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
-
         return;
     }
-
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -27,7 +26,13 @@ function stopPainting() {
     isPainting = false;
 }
 
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", movePainting);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
